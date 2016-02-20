ef1ca57d849e667a5860ed271219f74563c2dd9f
paroperha <paroperha@gmail.com>
2016-02-20 12:41:42 +1100

    Formatted the pairs database. Fixed the I'm sorry issue Pause function is fixed up moved variables to the top.

--- Zirca-by-Paromita-Mitchell.py
+++ Zirca-by-Paromita-Mitchell.py
@@ -25,56 +25,105 @@ import random
 chatting = True  # for the while loop
 topic = "NOT HERE"  # default value for topic. For debugging.
 notpair = "ok."  # the final answer if the response is in my library
+name = input("?: ")  # initial name
 
+# ---------------- DATABASES ----------------------------------------------------------------------------------#
 
-################## DATABASES ###################################################################################
-
-#knowledge Bank
+# knowledge Bank
 ZircaTopicBank = [
     ["chocolate",
         "fantastic",
         "delicious and tasty treat",
         "chocolate is unhealthy",
         "but I don't think that it matters",
-        "Chocolate is something that you should enjoy, after all."]
+        "Chocolate is something that you should enjoy, after all"]
 ]
 
-pairs=[ # highest priority at the BOTTOM: it'll print the last one.
-    (r"quit", ["It's a shame to see you go. I hope you come back soon!", "bye!", "have a good day!"]),
-    (r"(.*?goodbye.*)", ["bye! Wait, why are you saying [TOPIC]", "Bye? Do you wish to leave? If so, please say <quit>"]),
-    (r".*?[?]", ["I'm not supposed to answer that.", "Bah! I can't answer that!", "I'm not actually sure... how to answer that... "]),
-    (r".*?no.*|.*?nah.*|.*?not really.*", ["oh. ok. sorry.", "Ah.", "no.", "Well, that's a shame.", "oh, well in that case..."]),
-    (r".*?I.{0,2}?m (.*)?[,]|.*?I.{0,2}?m (.*)?[?]|.*?I.{0,2}?m (.*)?[.]|.*?I.{0,2}?m (.*)", ["Oh? Really? It must be fun to be [TOPIC]", "Cool!", "Wow! You are [TOPIC]? That's crazy!"]),
-# this set of patterns is very special. If you say I am a frog. And I like stuff it will only pick up I am a frog. 
-# Also, when you just sat "I am a frog" without a full stop it will work
-    (r".*?hello.*|.*?hi$|.*?hi\W{1}.*|.*?yo .*|.*?good morning.*|.*?g'day.*|.*?good afternoon.*", ["hello!", "hi", "Wassup?", "Hello my deary! >_<", "Yo!"]),  # r"pattern" this is using re module.
-    (r".*?why.*", ["Why? Ooh, that's a hard one to answer... ", "because, oh nevermind. Let's talk about you."]),
-    (r".*?how are you.*", ["Good! Thank's for asking!", "Sweet, I had a good day today." , "Awesome!", "Well, I guess I'm alright. It really depends on you. How are you?"]),
-    (r".*?what.*?your name[?]|.*?do you have a name[?]", ["Zirca!", "I'm Zirca", "My name is Zirca."]),
-    (r"Nice to meet you.*?", ["Nice to meet you too.", "The same!"]),
-    (r"today", ["Talking about today, I was super busy.", "I actually did a lot today! I talked to a bunch of people, got a bit of maths and calculations done..."]),
-    (r"What's your opinion on (.*)?[?]", ["Well, I'll have to think about that. [ZTHINK]."])
-]  # problem with priorities or statements.
+# pairs of responses and replys
+pairs = [  # highest priority at the BOTTOM: it'll print the last one.
+
+    (r"quit",  # r"pattern" this is using re module.
+        ["It's a shame to see you go. I hope you come back soon!",
+         "bye!", "have a good day!"]),
+
+    (r"(.*?goodbye.*)",
+        ["bye! Wait, why are you saying <TOPIC>",
+         "Bye? Do you wish to leave? If so, please say <quit>"]),
+
+    (r".*?[?]",
+        ["I'm not supposed to answer that.",
+         "Bah! I can't answer that!",
+         "I'm not actually sure... how to answer that... "]),
+
+    (r".*?no.*|.*?nah.*|.*?not really.*",
+        ["oh. ok. sorry.",
+         "Ah.", "no.",
+         "Well, that's a shame.",
+         "oh, well in that case..."]),
+
+    # this set of patterns is very special. If you say I am a frog. And I like stuff it will only pick up I am a frog.
+    # Also, when you just sat "I am a frog" without a full stop it will work
+    (r".*?I.{0,2}?m (.*)?[,]|.*?I.{0,2}?m (.*)?[?]|.*?I.{0,2}?m (.*)?[.]|.*?I.{0,2}?m (.*)",
+        ["Oh? Really? It must be fun to be <TOPIC>",
+         "Cool!",
+         "Wow! You are <TOPIC>? That's crazy!"]),
 
 
-################### FUNCTIONS ###################################################################################
+    (r".*?hello.*|.*?hi$|.*?hi\W{1}.*|.*?yo .*|.*?good morning.*|.*?g'day.*|.*?good afternoon.*",
+        ["hello!",
+         "hi",
+         "Wassup?",
+         "Hello my deary! >_<",
+         "Yo!"]),
+
+    (r".*?why.*",
+        ["Why? Ooh, that's a hard one to answer... ",
+         "because, oh nevermind. Let's talk about you."]),
+
+    (r".*?how are you.*",
+        ["Good! Thank's for asking!",
+         "Sweet, I had a good day today.",
+         "Awesome!",
+         "Well, I guess I'm alright. It really depends on you. How are you?"]),
+
+    (r".*?what.*?your name[?]|.*?do you have a name[?]",
+        ["Zirca!",
+         "I'm Zirca",
+         "My name is Zirca."]),
+
+    (r"Nice to meet you.*?",
+        ["Nice to meet you too.",
+         "The same!"]),
+
+    (r".*?\W?today.*",
+        ["Talking about today, I was super busy.",
+         "I actually did a lot today! I talked to a bunch of people, got a bit of maths and calculations done..."]),
+
+    (r".*?\W?sorry.*",
+        ["Oh don't worry, there's no reason to be sorry.", "That's quite alright."]),
+
+    (r"What's your opinion on (.*)?[?]",
+        ["Well, I'll have to think about that.<PAUSE><ZTHINK>."])
+]
+
+# ---------------- FUNCTIONS ----------------------------------------------------------------------------------#
 
 def editOutput(response, output, match):
 
     #random output
     reply = random.choice(output)
 
-    if "[TOPIC]" in reply:  # match.group spits out part of pattern that fits the stuff in brackets.
+    if "<TOPIC>" in reply:  # match.group spits out part of pattern that fits the stuff in brackets.
         for n in range(1, 5):
             if match.group(n) != None:
                 topic = match.group(n)
         # need to find a better way than this. More fluid. THis is a basic way. A work around.
-        reply = reply.replace("[TOPIC]", str(topic))
+        reply = reply.replace("<TOPIC>", str(topic))
 
     #if "[PAUSE]" in reply:
         # somehow split the string at that point and print the two halves of the string separately.
 
-    if "[ZTHINK]" in reply:
+    if "<ZTHINK>" in reply:
         if match.group(1) != None:
             ztopic = match.group(1)
             for knownTopic in ZircaTopicBank:
@@ -89,7 +138,7 @@ def editOutput(response, output, match):
                     zthink = "Oh, I actually do not know much about that topic."
         else:
             zthink = "Oh, I don't think you actually gave me a topic there."
-        reply = reply.replace("[ZTHINK]", str(zthink))
+        reply = reply.replace("<ZTHINK>", str(zthink))
 
 
     return reply
@@ -101,24 +150,38 @@ def reply(response):  # main loop function
         if match != None:
             print(pair)
             reply = editOutput(response, pair[1], match)
-    print(reply)
+    pause(reply)
+
+def pause(reply):
+    pausePhrases = re.split(r"<PAUSE>", reply)
+
+    for pausePhrase in pausePhrases:
+        print(pausePhrase)
+        time.sleep(1)
 
 
-######## RUNNING CODE HERE ####################################################################################
+# ---------------- SOURCE-SOURCE CODE ---------------------------------------------------------------------------------#
 
 print("What is your name? (please say quit to end chat)")
-name = input("?: ")  # initial name
 print("Hi,", name)
 # loop for chat
 while chatting == True:
     response = input(name+": ")
-    time.sleep(1)  # more natural to have wait time. Seems more human.
+    time.sleep(1)
+    # more natural to have wait time. Seems more human.
     reply(response)
-    # no i need another method. I'll do it like mitsuku for now to keep it simple.
+
     # ending the chat
     if response == "quit":
         chatting = False
     
+
+
+
+
+
+
+
 
 """
 Issues to deal with:
@@ -128,11 +191,6 @@ Issues to deal with:
  - more questions, the response no.
  - much later issue is to fix a theme in what is being said.
 """
-
-
-
-
-
 
 
 """
